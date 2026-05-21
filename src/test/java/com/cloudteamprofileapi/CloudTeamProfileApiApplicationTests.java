9056bac50789f355c7a4488a9e8f0106fa5887e6
Jiwon Yang <g.one9709@gmail.com>
2026-05-21 10:10:25 +0900

    chore: 로컬 개발 환경 설정

---
+++ src/test/java/com/cloudteamprofileapi/CloudTeamProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudteamprofileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTeamProfileApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
