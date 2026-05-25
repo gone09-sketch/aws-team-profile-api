64f883133ead6ccf5e2dc50ae516e280413d8c45
Jiwon Yang <g.one9709@gmail.com>
2026-05-26 02:05:10 +0900

    test: AWS S3 클라이언트 테스트 Mock 처리

--- src/test/java/com/cloudteamprofileapi/CloudTeamProfileApiApplicationTests.java
+++ src/test/java/com/cloudteamprofileapi/CloudTeamProfileApiApplicationTests.java
@@ -3,10 +3,19 @@ package com.cloudteamprofileapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CloudTeamProfileApiApplicationTests {
+
+    @MockitoBean
+    private S3Client s3Client;
+
+    @MockitoBean
+    private S3Presigner s3Presigner;
 
     @Test
     void contextLoads() {
